e4e5b4b8db56d49084bba46621fc667c9977182c
officialclymax-hue <officialclymax@gmail.com>
2025-09-08 03:13:22 +0100

    Update services.module.ts

--- src/modules/services/services.module.ts
+++ src/modules/services/services.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
 import { PrismaService } from '../../common/prisma.service';
-import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
-import { APP_GUARD, Reflector } from '@nestjs/core';
-import { RolesGuard } from '../../common/guards/roles.guard';
 
 @Module({
   controllers: [ServicesController],
-  providers: [
-    ServicesService, PrismaService,
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-    Reflector
-  ],
+  providers: [ServicesService, PrismaService],
 })
 export class ServicesModule {}
